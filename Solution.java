301aecc001ac381742ff632965578543c09e4f13
Puneet Sawhaney <153997477+puneetSawhaney@users.noreply.github.com>
2023-12-17 15:07:32 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,46 @@
+// Code by Puneet 
+
+class Solution {
+    class Pair implements Comparable<Pair>{
+
+        int val;
+        int idx;
+
+        Pair(int val, int idx){
+            this.val = val;
+            this.idx = idx;
+        }
+        public int compareTo(Pair o){
+            return this.val- o.val;
+        }
+    }
+
+
+    public int[] twoSum(int[] nums, int target) {
+
+        Pair[] pairArray = new Pair[nums.length];
+
+        for(int i =0; i <nums.length; i++){
+            pairArray[i] = new Pair(nums[i], i);
+        }
+
+        Arrays.sort(pairArray);
+        int li =0;
+        int ri = nums.length-1;
+        int[] res = new int[2];
+        while(li<ri){
+            int left = pairArray[li].val;
+            int right = pairArray[ri].val;
+            if(left+right < target){
+                li++;
+            }else if(left+right > target){
+                ri--;
+            }else{
+                res[0]= pairArray[li].idx;
+                res[1]= pairArray[ri].idx;
+                break;
+            }
+        }
+        return res;
+    }
+}
