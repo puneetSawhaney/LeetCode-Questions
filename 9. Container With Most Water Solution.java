40b5527f0410445bf4145bdafe25d08a0cf1085b
Puneet Sawhaney <153997477+puneetSawhaney@users.noreply.github.com>
2023-12-18 18:53:13 +0530

    Create 9. Container With Most Water Solution.java

---
+++ 9. Container With Most Water Solution.java
@@ -0,0 +1,27 @@
+// code by puneet
+// please visit explanation file to understand the approach I followed
+
+
+class Solution {
+    public int maxArea(int[] height) {
+
+        int res = 0;
+        int li =0;
+        int ri = height.length-1;
+
+        while(li<ri){
+            int left = height[li];
+            int right = height[ri];
+
+            int area = Math.min(left, right) * (ri-li);
+
+            res = Math.max(res,area);
+            if(left<right){
+                li++;
+            }else{
+                ri--;
+            }
+        }
+    return res;
+    }
+}
